21191447b34fc9781c49d4aa9de529d6fe6d7e5d
felipe-maia <fe_switch@hotmail.com>
2019-12-19 14:07:21 -0300

    -Firebase, testes

--- app/src/main/java/com/example/segurancabelica/MainActivity.java
+++ app/src/main/java/com/example/segurancabelica/MainActivity.java
@@ -4,11 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DatabaseReference usuarios = reference.child("usuarios");
+
+        Usuario user = new Usuario();
+        user.setNome("Felipe");
+        user.setPosto("SGT");
+
+        usuarios.child("001").setValue(user);
+
+
     }
 }
---
+++ app/src/main/java/com/example/segurancabelica/Usuario.java
@@ -0,0 +1,26 @@
+package com.example.segurancabelica;
+
+public class Usuario {
+
+    private String nome;
+    private String posto;
+
+    public Usuario() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getPosto() {
+        return posto;
+    }
+
+    public void setPosto(String posto) {
+        this.posto = posto;
+    }
+}
